53e3637592d41f36a9ce718d4c5ad6324536e7e6
Cory Endle <endle.cory@mayo.edu>
2021-12-02 08:29:21 -0600

    Removed the advanced search option

--- clientui/src/components/NcitSearch.vue
+++ clientui/src/components/NcitSearch.vue
@@ -36,16 +36,16 @@
 
         <!-- Search by uploading a file -->
         <div v-if="!searchByText" class="form-group col-md-12">
-          <textarea rows="5" cols="40" v-model="text" :disabled="true"></textarea>
+          <textarea rows="5" cols="40" v-model="textFromFile" :disabled="true"></textarea>
           <br>
-          <text-reader @load="text = $event"
+          <text-reader @load="textFromFile = $event"
             @clearText= "onClearText">
           </text-reader>
         </div>
       </div>
 
       <div class="form-row container">
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-4">
           <label for="inputIncludes">Includes</label>
           <select :disabled="!this.advanced" id="inputIncludes" class="form-control" v-model="includeSelected">
             <option
@@ -55,7 +55,7 @@
             </option>
           </select>
         </div>
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-4">
           <label for="inputType">Type</label>
           <select :disabled="!this.advanced" id="inputType" class="form-control" v-model="typeSelected">
             <option
@@ -65,7 +65,7 @@
             </option>
           </select>
         </div>
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-4">
           <label for="inputTerminology">Terminology</label>
           <select :disabled="!this.advanced" id="inputTerminology" class="form-control" v-model="terminologySelected">
             <option
@@ -76,7 +76,8 @@
           </select>
         </div>
 
-        <div class="form-group col-md-3">
+        <!-- Advanced Search Button -->
+        <!--div class="form-group col-md-3">
           <label for="searchButton">Simple/Advanced Search</label>
           <button
             id="advancedSearch"
@@ -89,97 +90,22 @@
             v-on:click="toggleButtonText()">
             {{button.text}}
           </button>
-        </div>
+        </div-->
       </div>
 
-      <div class="collapse" id="collapseExample">
+      <!--div class="collapse" id="collapseExample">
         <div class="form-row container">
-          <!--div class="form-group col-md-3">
-            <label for="inputAndOr">And/Or</label>
-            <select id="inputAndOr" class="form-control">
-              <option selected>And</option>
-              <option>And</option>
-              <option>Or</option>
-            </select>
-          </div-->
-          <div class="form-group col-md-14">
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-              <li class="nav-item">
-                <a class="nav-link active" id="concept-status-tab" data-toggle="tab" href="#concept-status" role="tab" aria-controls="concept-status" aria-selected="concept-status">Concept Status</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="definition-source-tab" data-toggle="tab" href="#definition-source" role="tab" aria-controls="definition-source" aria-selected="false">Definition Source</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="definition-type-tab" data-toggle="tab" href="#definition-type" role="tab" aria-controls="definition-type" aria-selected="false">Definition Type</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="property-tab" data-toggle="tab" href="#property-type" role="tab" aria-controls="property-type" aria-selected="false">Property</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="synonym-term-group-tab" data-toggle="tab" href="#synonym-term-group-type" role="tab" aria-controls="synonym-term-group-type" aria-selected="false">Synonym Term Group</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="synonym-type-tab" data-toggle="tab" href="#synonym-type-type" role="tab" aria-controls="synonym-type-type" aria-selected="false">Synonym Type</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="synonym-source-tab" data-toggle="tab" href="#synonym-source-type" role="tab" aria-controls="synonym-source-type" aria-selected="false">Synonym Source</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="synonym-type-list-tab" data-toggle="tab" href="#synonym-type-list-type" role="tab" aria-controls="synonym-type-list-type" aria-selected="false">Synonym Type List</a>
-              </li>
-            </ul>
-            <div class="tab-content" id="myTabContent">
-              <div class="tab-pane fade show active" id="concept-status" role="tabpanel" aria-labelledby="concept-status-tab">
-
-                <br><br>
-                <div class="container">
-                  <h5 id="example-5">Concept Status</h5>
-                  <!--div class="output">Data: {{ example5.value }}</div-->
-                  <Multiselect
-                    v-model="example5.value"
-                    v-bind="example5"
-                  ></Multiselect>
-
-                  <br><br>
-                </div>
-
-              </div>
-              <div class="tab-pane fade" id="definition-source" role="tabpanel" aria-labelledby="definition-source-tab">definition-source</div>
-              <div class="tab-pane fade" id="definition-type" role="tabpanel" aria-labelledby="definition-type-tab">definition-type</div>
-              <div class="tab-pane fade" id="property-type" role="tabpanel" aria-labelledby="property-type-tab">property-type</div>
-              <div class="tab-pane fade" id="synonym-term-group-type" role="tabpanel" aria-labelledby="synonym-term-group-type-tab">synonym-term-group-type</div>
-              <div class="tab-pane fade" id="synonym-type-type" role="tabpanel" aria-labelledby="synonym-type-type-tab">synonym-type</div>
-              <div class="tab-pane fade" id="synonym-source-type" role="tabpanel" aria-labelledby="synonym-source-type-tab">synonym-source</div>
-              <div class="tab-pane fade" id="synonym-type-list-type" role="tabpanel" aria-labelledby="synonym-type-list-type-tab">synonym-type-list</div>
-            </div>
-
+          Advanced search here...
         </div>
-
-        </div>
-      </div>
+      </div-->
     </form>
-
-    <!--div class="form-group col-md-3">
-      <button
-        id="advancedSearch"
-        class="btn btn-primary"
-        type="button"
-        data-toggle="collapse"
-        data-target="#collapseExample"
-        aria-expanded="false"
-        aria-controls="collapseExample"
-        v-on:click="toggleButtonText()">
-        {{button.text}}
-      </button>
-    </div-->
 
     <div class="form-group col-md-3">
       <button
         id="search"
         class="btn btn-primary"
         type="button"
-        :disabled='(searchByText && searchText.length == 0) || (!searchByText && text.length == 0)'
+        :disabled='(searchByText && searchText.length == 0) || (!searchByText && textFromFile.length == 0)'
         v-on:click="search()">
         Search
       </button>
@@ -196,7 +122,6 @@ import axios from 'axios'
 import { useLoading } from 'vue3-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
 import { notify } from "@kyvg/vue3-notification";
-import Multiselect from '@vueform/multiselect'
 import TextReader from "./TextReader";
 
 export default {
@@ -221,27 +146,8 @@ export default {
       terminologies: [],
       terminologySelected: "ncit",
 
-      text: "",
-
+      textFromFile: "",
       searchByText: true,
-
-      example5: {
-        mode: 'tags',
-        value: ['concept status 1'],
-        closeOnSelect: false,
-        options: [
-          { value: 'concept status 1', label: 'concept status 1' },
-          { value: 'concept status 2', label: 'concept status 2' },
-          { value: 'concept status 3', label: 'concept status 3' },
-          { value: 'concept status 4', label: 'concept status 4' },
-          { value: 'concept status 5', label: 'concept status 5' },
-          { value: 'concept status 6', label: 'concept status 6' },
-          { value: 'concept status 7', label: 'concept status 7' },
-          { value: 'concept status 8', label: 'concept status 8' },
-        ],
-        searchable: true,
-        createTag: true
-      },
     }
 
   },
@@ -250,7 +156,6 @@ export default {
   },
   components: {
       TextReader,
-      Multiselect,
   },
   metaInfo: {
     title: 'NCIt Search',
@@ -258,7 +163,7 @@ export default {
   methods: {
 
     onClearText: function() {
-      this.text=''
+      this.textFromFile=''
     },
     toggleButtonText: function() {
       this.advanced = !this.advanced;
@@ -282,20 +187,20 @@ export default {
         "Terminology : " + this.terminologySelected)
 
 
-        console.log(this.text)
+        console.log(this.textFromFile)
         notify({
           title: "Searching",
           text: "Searching and gathering results. Please wait.",
-          speed: 3000,
+          speed: 2000,
           type: "success"
         });
 
         // If the file has returns, convert them to commas
-        let trimmedFileText = this.text.split(/\r?\n|\r/g).join(',')
+        let trimmedFileText = this.textFromFile.split(/\r?\n|\r/g).join(',')
         console.log(trimmedFileText)
 
-        let loader = useLoading();
-        loader.show({
+      let $loading = useLoading();
+        const loader = $loading.show({
           // Optional parameters
           container: this.fullPage ? null : this.formContainer.value,
           canCancel: false,
@@ -317,7 +222,6 @@ export default {
               this.typeSelected  + '/'+
               trimmedFileText
         }
-
         console.log(
           searchUrl
         )
@@ -333,11 +237,13 @@ export default {
               fileLink.setAttribute('download', this.filename + '.' + this.extension);
               document.body.appendChild(fileLink);
               fileLink.click();
-
+loader.hide()
         }).catch(function(error) {
               console.error("Download Error: " + error);
               alert("Error Downloading file");
-        }).finally(function() { loader.hide()});
+        }).finally(function() {
+          loader.hide()
+        });
         // setTimeout(() => {
         //     loader.hide()
         //   },3000)
@@ -383,7 +289,6 @@ export default {
   border-color: rgb(0, 125, 188);
   color: white;
 }
-
 .btn-primary:hover, .btn-primary:focus, .btn-primary:active, .btn-primary.active, .open>.dropdown-toggle.btn-primary {
   background-color: rgb(0, 125, 188) !important;
 	border-color: rgb(0, 125, 188) !important;
